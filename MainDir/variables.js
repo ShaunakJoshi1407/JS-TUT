636a5f535eeb72d2b1c405577de36afccfb5381d
Shaunak Joshi <shaunakjoshi1407@gmail.com>
2024-04-02 02:05:44 +0000

    var and let

---
+++ MainDir/variables.js
@@ -0,0 +1,18 @@
+const accountId = 2
+let accountEmail = "newray@gmail.com"
+var psswd = "TrialBased"
+acctCity = "Pune"
+
+console.log([accountEmail, psswd, acctCity])
+
+/*
+A variable declared with var is defined throughout the program. 
+One of the issues with using the var keyword was redeclaring a variable inside a 
+block will also redeclare the variable outside the block. 
+
+
+var and let are both used for variable declaration in javascript but the 
+difference between them is that var is function scoped and let is block scoped.
+Variable declared by let cannot be redeclared and must be declared before use 
+whereas variables declared with var keyword are hoisted. 
+*/
